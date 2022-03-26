7a3ca6de83739aa8ba4a52bc86e4225f7ce9dcbe
joao-erik2077 <joao.crisostomo01@etec.sp.gov.br>
2022-03-26 13:39:55 -0300

    Changing homepage design

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  private taskList: any = [];
 
   constructor() {}
 
+  ngOnInit(): void {
+    for (let i = 1; i <= 15; i++) {
+      this.taskList[i - 1] = { name: `Example ${i}` };
+    }
+  }
 }
